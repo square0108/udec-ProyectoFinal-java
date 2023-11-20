f2bcfc37170915d5822591140448480942374d1d
square0108 <martinmariollanos@gmail.com>
2023-11-20 19:35:53 -0300

    Added: Parametros: TerrenoEnum tipoTerreno, Timer hambreTimer

--- src/main/java/Model/Especies/Animal.java
+++ src/main/java/Model/Especies/Animal.java
@@ -2,33 +2,53 @@ package Model.Especies;
 
 import Model.Alimento;
 import Model.Enumerations.AnimalEnum;
+import Model.Enumerations.TerrenoEnum;
+
+import java.util.Timer;
 
 public abstract class Animal {
+    private final TerrenoEnum tipoTerreno;
     private int porcentajeComida;
     private final int gananciaHambre;
     private final float rangoTemperatura[]; // celsius
+    private Timer hambreTimer;
 
-    public Animal(int gananciaHambre, float limInferiorTemperatura, float limSuperiorTemperatura) {
+    public Animal(TerrenoEnum tipoTerreno, int gananciaHambre, float limInferiorTemperatura, float limSuperiorTemperatura) {
+        this.tipoTerreno = tipoTerreno;
         this.rangoTemperatura = new float[]{limInferiorTemperatura, limSuperiorTemperatura};
         this.porcentajeComida = 100;
         this.gananciaHambre = gananciaHambre;
+
+        this.hambreTimer = new Timer();
     }
+
     public void comer(Alimento alimento) {
-        if (alimento != null && alimento.getConsumidoStatus() == false) {
+        if (alimento != null && !alimento.estaConsumido()) {
+            /* Alimentos siempre sacian el hambre completamente. */
             this.porcentajeComida = 100;
             alimento.setConsumidoStatus(true);
         }
     }
+
     public void ganarHambre() {
         porcentajeComida = porcentajeComida - gananciaHambre;
     }
 
     /**
-     * Este metodo es abstract ya que cada animal devuelve una lista de compatibilidad distinta.
+     * Regla importante para el funcionamiento del compatibility handling: todas las implementaciones de este metodo,
+     * deben tener a la misma subclase como la primera constante (indice 0) del arreglo retornado.
      * @return array de AnimalEnum conteniendo solo las constantes que representan los animales compatibles con este animal.
      */
-    public abstract AnimalEnum[] AnimalesCompatibles();
+    public abstract AnimalEnum[] animalesCompatibles();
 
-    /* TODO: void movement()?
-    */
+    public TerrenoEnum getTipoTerreno() {
+        return this.tipoTerreno;
+    }
+
+    public float[] getRangoTemperatura() {
+        return rangoTemperatura;
+    }
+    /* TODO: void movement()
+     */
 }
+
