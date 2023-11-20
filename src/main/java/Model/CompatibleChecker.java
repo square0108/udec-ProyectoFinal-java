db065f52e55b8cd1c7152cc11f48a31720d22ce8
square0108 <martinmariollanos@gmail.com>
2023-11-20 19:39:03 -0300

    Implementacion metodo isCompatible() para animal-animal y animal-habitat

--- src/main/java/Model/CompatibleChecker.java
+++ src/main/java/Model/CompatibleChecker.java
@@ -4,13 +4,26 @@ import Model.Enumerations.AnimalEnum;
 import Model.Especies.Animal;
 
 public class CompatibleChecker {
+    /* TODO: implementar compatibilidad entre animales tal que este metodo no requiera que animalesCompatibles[0] sea
+     *   siempre una constante especifica. */
     public static boolean isCompatible(Animal animal1, Animal animal2) {
-        AnimalEnum[] List = animal1.AnimalesCompatibles();
+        AnimalEnum[] List = animal1.animalesCompatibles();
 
         for (int i = 0; i < List.length; i++) {
-            if (List[i] == animal2.AnimalesCompatibles()[0]) return true;
+            if (List[i] == animal2.animalesCompatibles()[0]) return true;
         }
 
         return false;
+    }
+    public static boolean isCompatible(Animal animal, Habitat habitat) {
+        float animalMinTemp = animal.getRangoTemperatura()[0];
+        float animalMaxTemp = animal.getRangoTemperatura()[1];
+
+        if (animal.getTipoTerreno() == habitat.getTipoTerreno() && (animalMinTemp < habitat.getTemperatura() && habitat.getTemperatura() < animalMaxTemp)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
