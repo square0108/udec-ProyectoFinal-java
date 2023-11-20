80b69ed07c33b40cd020dd4e84c7de0aea0b3631
square0108 <martinmariollanos@gmail.com>
2023-11-20 19:34:16 -0300

    Metodo classToEnum, constructor con parametro anadido

--- src/main/java/Model/Enumerations/AnimalEnum.java
+++ src/main/java/Model/Enumerations/AnimalEnum.java
@@ -1,6 +1,22 @@
 package Model.Enumerations;
 
-public enum AnimalEnum {
-    LEON, HIPPO, COCODRILO, JIRAFA, ELEFANTE;
+import Model.Especies.*;
 
+public enum AnimalEnum {
+    LEON(Leon.class), JIRAFA(Jirafa.class), ELEFANTE(Elefante.class);
+
+    Class<?> claseAsociada;
+
+    AnimalEnum(Class<?> claseEspecie) {
+        this.claseAsociada = claseEspecie;
+    }
+
+    AnimalEnum classToEnum(Class<?> c) {
+        for (int i = 0; i < AnimalEnum.values().length; i++) {
+            if (c == AnimalEnum.values()[i].claseAsociada) return AnimalEnum.values()[i];
+        }
+        // todo: exception handling, o bien, por favor no usar este metodo es horrible
+        System.out.println("Ningun enum corresponde con esta clase.");
+        return null;
+    }
 }
