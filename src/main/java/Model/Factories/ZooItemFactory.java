30163a8a8cf1a581f7176463123b62de5394b2c7
square0108 <martinmariollanos@gmail.com>
2023-12-08 21:00:54 -0300

    feat: Factory crea instancias de Alimento

---
+++ src/main/java/Model/Factories/ZooItemFactory.java
@@ -0,0 +1,40 @@
+package Model.Factories;
+
+import Model.Alimento;
+import Model.Animal;
+import Model.EntornosHabitat.Jungla;
+import Model.Enumerations.*;
+import Model.Especies.*;
+import Model.Exceptions.AnimalNoExisteException;
+import Model.Habitat;
+
+public class ZooItemFactory {
+    public Animal newAnimalInstance(EspeciesEnum animal) throws AnimalNoExisteException {
+        return switch (animal) {
+            case JIRAFA -> new Jirafa();
+            case ELEFANTE -> new Elefante();
+            case LEON -> new Leon();
+            default -> throw new AnimalNoExisteException();
+        };
+    }
+
+    public Habitat newHabitatInstance(HabitatEnum hab) {
+        return switch (hab) {
+            case JUNGLA -> new Jungla();
+            default -> null;
+        };
+    }
+
+    /**
+     * En un principio se pensaba tener distintos tipos de alimento, decidimos no hacer esto, pero se dejó con un enum en caso de querer implementarlo a futuro.
+     * @param tipoComida dieta de comida a crear
+     * @return nueva instancia de Alimento usable en Model y View
+     */
+    public Alimento newAlimentoInstance(ComidaEnum tipoComida) {
+        return switch (tipoComida) {
+            case CARNE -> new Alimento();
+            case HENO -> new Alimento();
+            default -> null;
+        };
+    }
+}
